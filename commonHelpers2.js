1d7017e2d8ea5705dbecbe4714d43c937a42c55f
Yana17-ratsa <142500676+Yana17-ratsa@users.noreply.github.com>
2024-03-12 07:21:53 +0000

    Deploying to gh-pages from @ Yana17-ratsa/goit-js-hw-09@7671361271005d191c3e4bc1ae2e837648c7a9dc 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const e=document.querySelector(".feedback-form");e.elements.email.value;const n=e.elements.message.value;console.log(n);e.addEventListener("input",t);function t(o){}
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const e=document.querySelector(".feedback-form");e.addEventListener("input",m);function m(t){emailElem,messageElem}
 //# sourceMappingURL=commonHelpers2.js.map
